454358b0145253ec4cd80238fba72cc29b41b931
Vergell <VergellHub@users.noreply.github.com>
2021-03-03 18:35:04 +0300

    Карту скинул

---
+++ Assets/Ivanko/Scripts/CharacterControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public float speed = 4.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDir = Vector3.zero;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            moveDir.y = jumpSpeed;
+        }
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+        
+
+    }
+}
